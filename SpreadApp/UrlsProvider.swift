b6bdf4cf5f8f9fc13e7f4fe13e0ca27bffcb1dfc
Frank Bassard <f.bassard@gmail.com>
2014-12-27 14:27:43 +1300

    First page and more Model

--- SpreadApp/UrlsProvider.swift
+++ SpreadApp/UrlsProvider.swift
@@ -11,4 +11,23 @@ import UIKit
 class UrlsProvider: NSObject {
     let baseUrl = "http://localhost:8080/"
     
+    func getUserById (id : String) -> String {
+        return baseUrl + "users/" + id
+    }
+    
+    func createUser () -> String {
+        return baseUrl + "users"
+    }
+    
+    func modifyUser (id : String) -> String {
+        return baseUrl + "users/" + id
+    }
+    
+    func deleteUser (id : String) -> String {
+        return baseUrl + "users/" + id
+    }
+    
+    func addPosition (id : String) -> String {
+        return baseUrl + "users/" + id + "/positions"
+    }
 }
