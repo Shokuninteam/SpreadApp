791e89a38e9555572bda6be33b84cb87ab40a3b7
Frank Bassard <f.bassard@gmail.com>
2014-12-27 13:25:42 +1300

    Sign in V0

---
+++ SpreadApp/RequestsServices.swift
@@ -0,0 +1,13 @@
+//
+//  RequestsServices.swift
+//  SpreadApp
+//
+//  Created by Frank Bassard on 27/12/2014.
+//  Copyright (c) 2014 Shokunin. All rights reserved.
+//
+
+import UIKit
+
+class RequestsServices: NSObject {
+   
+}
--- SpreadApp/StartPageViewController.swift
+++ SpreadApp/StartPageViewController.swift
@@ -1,32 +1,26 @@
 //
-//  LoginController.swift
+//  StartPageViewController.swift
 //  SpreadApp
 //
-//  Created by Mohamed Mokhtari on 26/12/2014.
+//  Created by Frank Bassard on 27/12/2014.
 //  Copyright (c) 2014 Shokunin. All rights reserved.
 //
 
 import UIKit
 
-class LoginController: UIViewController {
-    
-    @IBAction func SignIn(sender: UIButton) {
-        
-    }
+class StartPageViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     override func viewWillAppear(animated: Bool) {
-        self.navigationController?.navigationBar.hidden = false
+        self.navigationController?.navigationBar.hidden = true
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    
-}
 
+}
