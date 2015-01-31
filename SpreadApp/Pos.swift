1ca52e7271fa0035e2ada07b2561e3644dbd98d3
Frank Bassard <f.bassard@gmail.com>
2015-01-31 21:01:01 +1300

    minor design

--- SpreadApp/Pos.swift
+++ SpreadApp/Pos.swift
@@ -1,11 +1,3 @@
-//
-//  Pos.swift
-//  SpreadApp
-//
-//  Created by Frank Bassard on 27/12/2014.
-//  Copyright (c) 2014 Shokunin. All rights reserved.
-//
-
 import UIKit
 
 class Pos: NSObject {
