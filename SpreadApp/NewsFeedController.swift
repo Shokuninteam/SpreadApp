4990a8cdfe47c0c8fb513af63267271eb1572569
Frank Bassard <f.bassard@gmail.com>
2015-02-06 12:00:15 +1300

    No notes to display overlay

--- SpreadApp/NewsFeedController.swift
+++ SpreadApp/NewsFeedController.swift
@@ -9,6 +9,7 @@ class NewsFeedController: UIPageTargetViewController, FeedRequestDelegate, CLLoc
     var currentNote : Note?
     var author : User?
     var location = false
+    var overlayView : UIView?
     
     @IBOutlet var avatar: UIImageView!
     @IBOutlet var authorName: UILabel!
@@ -63,8 +64,56 @@ class NewsFeedController: UIPageTargetViewController, FeedRequestDelegate, CLLoc
     
     func nextNote (){
         if self.unansweredNotes != nil && self.unansweredNotes?.count > 0 {
+            self.overlayView?.hidden = true
             self.currentNote = self.unansweredNotes!.removeLast() as Note
             displayNote(self.currentNote!)
+        } else {
+            noNotesOverlay();
+        }
+    }
+    
+    func noNotesOverlay () {
+        if !UIAccessibilityIsReduceTransparencyEnabled() {
+            if (self.overlayView == nil){
+                // blur view
+                let blurEffect = UIBlurEffect(style: UIBlurEffectStyle.ExtraLight)
+                let blurEffectView = UIVisualEffectView(effect: blurEffect)
+                blurEffectView.frame = view.bounds
+                self.overlayView=blurEffectView
+                view.addSubview(blurEffectView)
+                
+                // vibrancy view
+                var vibrancy = UIVibrancyEffect(forBlurEffect: blurEffect)
+                var vibrancyView = UIVisualEffectView(effect: vibrancy)
+                vibrancyView.frame = CGRect(x: 0, y: 0, width: blurEffectView.frame.size.width, height:blurEffectView.frame.size.height)
+                
+                // label
+                var label = UILabel(frame: CGRect(x: vibrancyView.frame.size.width / 2 - 100, y: vibrancyView.frame.size.height / 2 - 100, width: 200, height: 100))
+                label.textAlignment = NSTextAlignment.Center
+                label.text = "Nothing new to display"
+                label.font = UIFont(name: label.font.fontName, size: 14)
+                label.textColor = UIColor.grayColor()
+                vibrancyView.addSubview(label)
+                
+                // label
+                var label2 = UILabel(frame: CGRect(x: vibrancyView.frame.size.width / 2 - 100, y: vibrancyView.frame.size.height / 2 - 50, width: 200, height: 100))
+                label2.textAlignment = NSTextAlignment.Center
+                label2.text = "<- Profil      Notes ->"
+                label2.font = UIFont(name: label.font.fontName, size: 14)
+                label2.textColor = UIColor.grayColor()
+                vibrancyView.addSubview(label2)
+                blurEffectView.addSubview(vibrancyView)
+                
+                //add auto layout constraints so that the blur fills the screen upon rotating device
+                blurEffectView.setTranslatesAutoresizingMaskIntoConstraints(false)
+                view.addConstraint(NSLayoutConstraint(item: blurEffectView, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: view, attribute: NSLayoutAttribute.Top, multiplier: 1, constant: 0))
+                view.addConstraint(NSLayoutConstraint(item: blurEffectView, attribute: NSLayoutAttribute.Bottom, relatedBy: NSLayoutRelation.Equal, toItem: view, attribute: NSLayoutAttribute.Bottom, multiplier: 1, constant: 0))
+                view.addConstraint(NSLayoutConstraint(item: blurEffectView, attribute: NSLayoutAttribute.Leading, relatedBy: NSLayoutRelation.Equal, toItem: view, attribute: NSLayoutAttribute.Leading, multiplier: 1, constant: 0))
+                view.addConstraint(NSLayoutConstraint(item: blurEffectView, attribute: NSLayoutAttribute.Trailing, relatedBy: NSLayoutRelation.Equal, toItem: view, attribute: NSLayoutAttribute.Trailing, multiplier: 1, constant: 0))
+            } else {
+                self.overlayView?.hidden = false
+            }
+            
         }
     }
     
